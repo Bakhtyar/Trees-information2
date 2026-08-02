4be6788e8e8984be52747ca3ba2c19dd99de79a2
Bakhtyar <141077047+Bakhtyar@users.noreply.github.com>
2026-08-02 23:26:00 +0300

    Set base path for Vite configuration
    Added base path to resolve white page issue.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Trees-information2/', // 👈 تم إضافة هذا السطر هنا لحل مشكلة الصفحة البيضاء
     plugins: [
       react(), 
       tailwindcss(),
